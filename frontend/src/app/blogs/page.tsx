025e35b3c8682773f884fa93cd7e885c4dd84d13
arian <arian.maki@yahoo.com>
2026-05-28 15:25:48 +0200

    feat: data fetching and types defiend for blogs (#10)

--- frontend/src/app/blogs/page.tsx
+++ frontend/src/app/blogs/page.tsx
@@ -1,4 +1,24 @@
-function Page() {
-  return <div>page</div>;
+import { Post } from "../../types/post";
+
+type PostsResponse = {
+  data: {
+    posts: Post[];
+  };
+};
+
+async function Page() {
+  await new Promise<void>((res) => setTimeout(() => res(), 3000));
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
+  const {
+    data: { posts },
+  }: PostsResponse = await res.json();
+  console.log(posts);
+  return (
+    <div>
+      {posts.map((post) => {
+        return <div>{post.title}</div>;
+      })}
+    </div>
+  );
 }
 export default Page;
